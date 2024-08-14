76975037243e098ee757975ca56ac333d6c835bf
vivekthorat73 <120593033+vivekthorat73@users.noreply.github.com>
2024-08-14 23:03:23 +0530

    Project structure 01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeadbarComponent } from './headbar/headbar.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true,
+  // template: `
+  //             <app-headbar></app-headbar>
+  //             <app-navbar></app-navbar>`,
+  imports: [RouterOutlet, NavbarComponent, HeadbarComponent, HomepageComponent]
 })
 export class AppComponent {
   title = 'adaptive-web-app-builder';
-}
+} 
